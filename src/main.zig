03907d08a19fadf97e80986a87464bb80008652f
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-05 07:23:27 +0000

    feat: add argument validation and help message

--- src/main.zig
+++ src/main.zig
@@ -3,4 +3,13 @@ const std = @import("std");
 pub fn main() !void {
     const args = try std.process.argsAlloc(std.heap.page_allocator);
     defer std.process.argsFree(std.heap.page_allocator, args);
+
+    var stderr_buffer: [1024]u8 = undefined;
+    var stderr_writer = std.fs.File.stderr().writer(&stderr_buffer);
+    const stderr = &stderr_writer.interface;
+    if (args.len < 2 or args.len > 3 or (std.mem.eql(u8, args[1], "--help") or std.mem.eql(u8, args[1], "-h"))) {
+        try stderr.writeAll("Usage: fancy-cat <path-to-pdf> <optional-page-number>\n");
+        try stderr.flush();
+        return;
+    }
 }
