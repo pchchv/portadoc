063a3741571628f9216447174a458ac7493ab491
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-05 07:38:09 +0000

    feat: parallelize mupdf compilation

--- build.zig
+++ build.zig
@@ -7,6 +7,10 @@ pub fn build(b: *std.Build) void {
     defer make_args.deinit(allocator);
 
     make_args.append(allocator, "make") catch unreachable;
+
+    // use as many cores as possible by default (like zig) I dont know how to check for j<N> arg
+    const cpu_count = std.Thread.getCpuCount() catch 1;
+    make_args.append(allocator, b.fmt("-j{d}", .{cpu_count})) catch unreachable;
     make_args.append(allocator, "-C") catch unreachable;
     make_args.append(allocator, "deps/mupdf") catch unreachable;
     if (target.result.os.tag == .linux) {
