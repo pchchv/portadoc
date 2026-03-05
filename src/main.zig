aa8b7a4c839395807ba18fa92cb77bddff9219ff
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-05 07:22:17 +0000

    feat: add boilerplate for command-line argument parsing

---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    defer std.process.argsFree(std.heap.page_allocator, args);
+}
