4e27a0c422557fc359255d4aec00d3d527e4cab7
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-05 07:48:00 +0000

    feat: add dynamic mupdf linking support and Homebrew paths

--- build.zig
+++ build.zig
@@ -8,6 +8,30 @@ fn addMupdfStatic(exe: *std.Build.Step.Compile, b: *std.Build, prefix: []const u
     exe.linkLibC();
 }
 
+fn addMupdfDynamic(exe: *std.Build.Step.Compile, target: std.Target) void {
+    if (target.os.tag == .macos and target.cpu.arch == .aarch64) {
+        exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
+        exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
+    } else if (target.os.tag == .macos and target.cpu.arch == .x86_64) {
+        exe.addIncludePath(.{ .cwd_relative = "/usr/local/include" });
+        exe.addLibraryPath(.{ .cwd_relative = "/usr/local/lib" });
+    } else if (target.os.tag == .linux) {
+        exe.addIncludePath(.{ .cwd_relative = "/home/linuxbrew/.linuxbrew/include" });
+        exe.addLibraryPath(.{ .cwd_relative = "/home/linuxbrew/.linuxbrew/lib" });
+
+        const linux_libs = [_][]const u8{
+            "mupdf-third", "harfbuzz",
+            "freetype",    "jbig2dec",
+            "jpeg",        "openjp2",
+            "gumbo",       "mujs",
+        };
+        for (linux_libs) |lib| exe.linkSystemLibrary(lib);
+    }
+    exe.linkSystemLibrary("mupdf");
+    exe.linkSystemLibrary("z");
+    exe.linkLibC();
+}
+
 pub fn build(b: *std.Build) void {
     const prefix = "./local";
     const target = b.standardTargetOptions(.{});
