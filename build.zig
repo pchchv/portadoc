8edbd36bfd51ea20d2249f037925ca235b1e395b
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-05 07:37:28 +0000

    feat: add mupdf build integration

---
+++ build.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const allocator = std.heap.page_allocator;
+    var make_args: std.ArrayList([]const u8) = .empty;
+    defer make_args.deinit(allocator);
+
+    make_args.append(allocator, "make") catch unreachable;
+    make_args.append(allocator, "-C") catch unreachable;
+    make_args.append(allocator, "deps/mupdf") catch unreachable;
+    if (target.result.os.tag == .linux) {
+        make_args.append(allocator, "HAVE_X11=no") catch unreachable;
+        make_args.append(allocator, "HAVE_GLUT=no") catch unreachable;
+    }
+}
