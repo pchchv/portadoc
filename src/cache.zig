24992f11d8dfd86ba14de292147355aec67d5cbb
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-07 05:14:43 +0000

    feat: implement LRU Cache initialization/deinitialization and memory management

--- src/cache.zig
+++ src/cache.zig
@@ -1,4 +1,7 @@
+const Self = @This();
+const std = @import("std");
 const vaxis = @import("vaxis");
+const Config = @import("config/config.zig");
 
 pub const CachedImage = struct { image: vaxis.Image };
 pub const Key = struct {
@@ -16,3 +19,43 @@ const Node = struct {
     prev: ?*Node,
     next: ?*Node,
 };
+
+allocator: std.mem.Allocator,
+map: std.AutoHashMap(Key, *Node),
+head: ?*Node,
+tail: ?*Node,
+config: *Config,
+lru_size: u16,
+vx: vaxis.Vaxis,
+tty: *const vaxis.Tty,
+
+pub fn init(
+    allocator: std.mem.Allocator,
+    config: *Config,
+    vx: vaxis.Vaxis,
+    tty: *const vaxis.Tty,
+) Self {
+    return .{
+        .allocator = allocator,
+        .map = std.AutoHashMap(Key, *Node).init(allocator),
+        .head = null,
+        .tail = null,
+        .config = config,
+        .lru_size = config.cache.lru_size,
+        .vx = vx,
+        .tty = tty,
+    };
+}
+
+pub fn deinit(self: *Self) void {
+    var current = self.head;
+    while (current) |node| {
+        const next = node.next;
+
+        self.allocator.destroy(node);
+
+        current = next;
+    }
+
+    self.map.deinit();
+}
