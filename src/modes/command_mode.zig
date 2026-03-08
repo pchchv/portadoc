de1c1ca0774cd7b1065e4ab6b51b9732b76600b5
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-08 07:05:31 +0000

    feat(command mode): implement key handling

--- src/modes/command_mode.zig
+++ src/modes/command_mode.zig
@@ -1,6 +1,7 @@
 const Self = @This();
 const std = @import("std");
 const vaxis = @import("vaxis");
+const Config = @import("../config/config.zig");
 const Context = @import("../context.zig").Context;
 
 const TextInput = vaxis.widgets.TextInput;
@@ -87,4 +88,83 @@ fn handleQuit(self: *Self, cmd: []const u8) bool {
 
     self.context.should_quit = true;
     return true;
+}
+
+pub fn handleKeyStroke(self: *Self, key: vaxis.Key, km: Config.KeyMap) !void {
+    if (key.matches(km.exit_command_mode.codepoint, km.exit_command_mode.mods) or
+        (key.matches(vaxis.Key.backspace, .{}) and self.text_input.buf.realLength() == 0))
+    {
+        self.context.changeMode(.view);
+        return;
     }
+
+    if (key.matches(km.execute_command.codepoint, km.execute_command.mods)) {
+        const text_input = try self.text_input.buf.toOwnedSlice();
+        defer self.context.allocator.free(text_input);
+
+        const cmd = std.mem.trim(u8, text_input, &std.ascii.whitespace);
+
+        if (cmd.len > 0) {
+            self.executeCommand(cmd);
+            self.context.history.addToHistory(cmd);
+        }
+
+        self.context.changeMode(.view);
+        return;
+    }
+
+    // history
+    if (key.matches(km.history_back.codepoint, km.history_back.mods) or key.matches(km.history_forward.codepoint, km.history_forward.mods)) {
+        if (self.history_prefix == null) {
+            const text_input = try self.text_input.buf.toOwnedSlice();
+            defer self.context.allocator.free(text_input);
+            self.history_prefix = try self.context.allocator.dupe(u8, text_input);
+        }
+
+        const history_prefix = self.history_prefix.?;
+        var filtered = std.ArrayList([]const u8){};
+        defer filtered.deinit(self.context.allocator);
+
+        for (self.context.history.items.items) |cmd| {
+            if (std.mem.startsWith(u8, cmd, history_prefix)) {
+                try filtered.append(self.context.allocator, cmd);
+            }
+        }
+
+        const count = @as(isize, @intCast(filtered.items.len));
+        if (count > 0) {
+            if (key.matches(km.history_back.codepoint, km.history_back.mods)) {
+                if (self.context.history.index == -1) {
+                    self.context.history.index = count - 1;
+                } else if (self.context.history.index > 0) {
+                    self.context.history.index -= 1;
+                }
+            } else if (key.matches(km.history_forward.codepoint, km.history_forward.mods)) {
+                if (self.context.history.index >= 0 and self.context.history.index < count - 1) {
+                    self.context.history.index += 1;
+                } else {
+                    self.context.history.index = -1;
+                }
+            }
+        }
+
+        const input_to_display = if (self.context.history.index == -1 or count == 0)
+            history_prefix
+        else
+            filtered.items[@as(usize, @intCast(self.context.history.index))];
+
+        self.text_input.buf.clearRetainingCapacity();
+        self.text_input.reset();
+        try self.text_input.insertSliceAtCursor(input_to_display);
+
+        return;
+    }
+
+    if (self.history_prefix) |history_prefix| {
+        self.context.allocator.free(history_prefix);
+        self.history_prefix = null;
+    }
+    self.context.history.index = -1;
+
+    try self.text_input.update(.{ .key_press = key });
+}
