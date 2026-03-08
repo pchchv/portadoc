bb3adefcfe16fce67802b9c40f901203010ffb38
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-08 07:10:05 +0000

    feat(view mode): define key action structure

--- src/modes/view_mode.zig
+++ src/modes/view_mode.zig
@@ -1,7 +1,14 @@
 const Self = @This();
+const vaxis = @import("vaxis");
 const Context = @import("../context.zig").Context;
 
 context: *Context,
+
+pub const KeyAction = struct {
+    codepoint: u21,
+    mods: vaxis.Key.Modifiers,
+    handler: *const fn (*Context) void,
+};
 
 pub fn init(context: *Context) Self {
     return .{
